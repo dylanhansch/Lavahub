0b106be29364ba4c492d382d7128d1526011e556
Dylan Hansch <dylan@dylanhansch.net>
2014-01-30 20:52:12 -0600

    Add enable/disable

--- src/main/java/net/dylanhansch/lavahub/Lavahub.java
+++ src/main/java/net/dylanhansch/lavahub/Lavahub.java
@@ -1,7 +1,24 @@
 package net.dylanhansch.lavahub;
 
+import java.util.logging.Logger;
+
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Lavahub extends JavaPlugin{
+	public final Logger logger = Logger.getLogger("Minecraft");
+	public static Lavahub plugin;
+	
+	@Override
+	public void onEnable() {
+		PluginDescriptionFile pdfFile = this.getDescription();
+		this.logger.info(pdfFile.getName() + " Has been enabled.");
+	}
+	
+	@Override
+	public void onDisable() {
+		PluginDescriptionFile pdfFile = this.getDescription();
+		this.logger.info(pdfFile.getName() + " Has been disabled.");
+	}
 
 }
