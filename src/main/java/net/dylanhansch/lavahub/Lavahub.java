9e74450dbf5572d042f876c066f9b5fcd4d3c1c4
Dylan Hansch <dylan@dylanhansch.net>
2014-03-18 13:16:53 -0500

    Essentials as main

--- src/main/java/net/dylanhansch/lavahub/Lavahub.java
+++ src/main/java/net/dylanhansch/lavahub/Lavahub.java
@@ -1,15 +1,9 @@
 package net.dylanhansch.lavahub;
 
-import net.dylanhansch.lavahub.command.ActionCommand;
-import net.dylanhansch.lavahub.command.ClearInventoryCommand;
-import net.dylanhansch.lavahub.command.GamemodeCommand;
-import net.dylanhansch.lavahub.command.HackCommand;
-import net.dylanhansch.lavahub.command.PingCommand;
 import net.dylanhansch.lavahub.command.RawmsgCommand;
 import net.dylanhansch.lavahub.command.SayCommand;
 import net.dylanhansch.lavahub.command.SetSpawnCommand;
 import net.dylanhansch.lavahub.command.SpawnCommand;
-import net.dylanhansch.lavahub.command.TimeCommand;
 
 import org.bukkit.Location;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -23,14 +17,8 @@ public class Lavahub extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		saveDefaultConfig();
-		getCommand("action").setExecutor(new ActionCommand(this));
-		getCommand("clearinventory").setExecutor(new ClearInventoryCommand(this));
-		getCommand("gamemode").setExecutor(new GamemodeCommand(this));
-		getCommand("hack").setExecutor(new HackCommand(this));
-		getCommand("ping").setExecutor(new PingCommand(this));
 		getCommand("rawmsg").setExecutor(new RawmsgCommand(this));
 		getCommand("say").setExecutor(new SayCommand(this));
-		getCommand("time").setExecutor(new TimeCommand(this));
 		getCommand("spawn").setExecutor(new SpawnCommand(this));
 		getCommand("setspawn").setExecutor(new SetSpawnCommand(this));
 	}
